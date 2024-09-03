9e57a341ef31b6a4c487ea9fe1c1415cefa231c9
Drew D <dgdavid.dev@gmail.com>
2024-09-03 16:30:27 +1000

    add tabaccord component, init reactmarkdown

--- ma-project/src/App.js
+++ ma-project/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './components/Hero';
 import Cards from './components/Cards';
+import TabsAccordionComponent from './components/TabAccord';
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <Cards />
+      <TabsAccordionComponent />
     </div>
   );
 }

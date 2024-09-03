5ae439962519743f9aae2a40755cc1a4cb38bee8
Drew D <dgdavid.dev@gmail.com>
2024-09-03 18:31:56 +1000

    remove markdown, setInnerHTML of TabAccord
    Add simple css styles for data html

--- ma-project/src/components/TabAccord.js
+++ ma-project/src/components/TabAccord.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import ReactMarkdown from 'react-markdown';
 import data from '../data.json';
-
-console.log(ReactMarkdown)
 
 const TabsAccordionComponent = () => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -22,27 +19,27 @@ const TabsAccordionComponent = () => {
 
     setActiveIndex(index === activeIndex ? null : index);
   }
-  ;
+;
 
   return (
     <div className="w-full max-w-lg mx-auto p-4 pb-10">
       {!isMobileView ? (
         <>
-          <div className="flex border-b border-gray-200">
+          <div className="flex border border-gray-200">
             {data.map((item, index) => (
               <button
                 key={item.id}
                 onClick={() => handleItemClick(index)}
                 className={`flex-1 py-2 px-4 text-center ${
-                  activeIndex === index ? 'border-b-2 border-blue-500 text-blue-500' : 'text-gray-600'
+                  activeIndex === index ? 'font-bold border-b-2 border-blue-500 text-blue-500' : 'font-bold text-gray-600'
                 }`}
               >
                 {item.title}
               </button>
             ))}
           </div>
-          <div className="p-4 prose text-gray-700 border border-t-0 border-gray-200">
-            <ReactMarkdown children={activeIndex !== null ? data[activeIndex].content : ''} />
+          <div className="p-4 text-gray-700 border border-t-0 border-gray-200">
+          <div className="data__html" dangerouslySetInnerHTML={{ __html: activeIndex !== null && data[activeIndex].content }}></div>
           </div>
         </>
       ) : (
@@ -52,15 +49,15 @@ const TabsAccordionComponent = () => {
               <div key={item.id}>
                 <button
                   onClick={() => handleItemClick(index)}
-                  className={`w-full text-left py-2 px-4 border-b border-gray-200 ${
-                    activeIndex === index ? 'bg-blue-100' : ''
+                  className={`font-bold w-full text-left py-2 px-4 border-b border-gray-200 ${
+                    activeIndex === index ? 'font-bold bg-blue-100' : ''
                   }`}
                 >
                   {item.title}
                 </button>
                 {activeIndex === index && (
-                  <div className="p-4 prose text-gray-700 border-b border-gray-200">
-                    <ReactMarkdown children={item.content} />
+                  <div className="p-4 text-gray-700 border-b border-gray-200">
+                    <div className="data__html" dangerouslySetInnerHTML={{ __html: item.content }}></div>
                   </div>
                 )}
               </div>
@@ -73,3 +70,5 @@ const TabsAccordionComponent = () => {
 };
 
 export default TabsAccordionComponent;
+
+
